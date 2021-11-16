6ab4caa5229161138eb35ce3e12ba831939a9ce2
someshwaran <92450817+Someshwaranmuralikumar@users.noreply.github.com>
2021-11-16 16:18:16 +0530

    Add files via upload

---
+++ IfStatements/A-1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace A_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Salary;
+            double total,hrd,da;
+            Console.WriteLine("Enter the basic salary : ");
+            Salary = Convert.ToInt32(Console.ReadLine());
+            if(Salary<=10000)
+            {
+                da = Salary*0.8;
+                hrd = Salary*0.2;
+            }
+            else if(Salary<=20000)
+            {
+                da = Salary*0.9;
+                hrd = Salary*0.25;
+            }
+            else
+            {
+                da = Salary*0.95;
+                hrd = Salary*0.3;
+            }
+            total = Salary+da+hrd;
+            Console.WriteLine("Your gross salary is "+total);            
+
+        }
+    }
+}
---
+++ IfStatements/marks/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace marks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your marks (out of 100) : ");
+            int mark = Convert.ToInt32(Console.ReadLine());
+            if(mark>=0 && mark<=100)
+            {
+                if(mark>=90)
+                    Console.WriteLine("Grade 'A' ");
+                else if(mark>=80)
+                    Console.WriteLine("Grade 'B' ");
+                else if(mark>=70)
+                    Console.WriteLine("Grade 'C' ");
+                else if(mark>=60)
+                    Console.WriteLine("Grade 'D' ");
+                else if(mark>=40)
+                    Console.WriteLine("Grade 'E' ");
+                else
+                    Console.WriteLine("Grade 'Fail' ");
+            }
+            else
+            {
+                Console.WriteLine("PLease try again mark to be between 0 and 100!!!!!");
+            }
+
+        }
+    }
+}
