11a2e50249cb3d73a3c159f0bf192dba4d44d616
someshwaran <92450817+Someshwaranmuralikumar@users.noreply.github.com>
2021-11-16 16:22:27 +0530

    Add files via upload

---
+++ Star pattern/Basic star/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Basic_star
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            hi();
+            hello();
+        }
+        static void hi()
+        {
+            Console.WriteLine("Star Pattern -1 \nA single line in vertical");
+            int i;
+            for(i=0;i<6;i++)
+            {
+                Console.WriteLine("*");
+            }
+            Console.ReadLine();
+        }
+        static void hello()
+        {
+            Console.WriteLine("Star Pattern -2 \n To build a square");
+            int row,col;
+            for(row=0;row<4;row++)
+            {
+                for(col=0;col<4;col++)
+                {
+                    Console.WriteLine("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Star pattern/RigtAngle/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace RigtAngle
+{
+    class A
+    {
+        public static void xyz()
+        {
+           Console.WriteLine(" ");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i,j;
+            for(i=1;i<=5;i++)
+            {
+                for(j=1;j<5;j++)
+                {
+                    Console.Write(" ");
+                }
+                for(j=1;j<=i;j++)
+                {
+                    if(i==5||j==1||j==i)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
