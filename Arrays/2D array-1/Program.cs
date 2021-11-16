334d5c737f283a48796a91ec424c529b2d6dcb1a
someshwaran <92450817+Someshwaranmuralikumar@users.noreply.github.com>
2021-11-16 16:30:48 +0530

    Add files via upload

---
+++ Arrays/2D array-1/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _2D_array_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] arr1 = {9,4,2,54,1};
+            int[] arr2 = {1,5,63,2,8};
+            min(arr1);
+            min(arr2);
+            max(arr1);
+            max(arr2);
+            array(arr1);
+            array(arr2);
+            Length(arr1);
+            Length(arr2);
+            jagged();
+        }
+        static void array(int[] arr)
+        {
+            Console.WriteLine("Print the array elements :  ");
+            for(int i=0;i<arr.Length;i++)
+            {
+                Console.Write(arr[i]);
+            }
+        }
+        static void min(int[] arr)
+        {
+            int min = arr[0];
+            for(int i=1;i<arr.Length;i++)
+            {
+                if(min > arr[i])
+                {
+                    min = arr[i];
+                }
+            }
+            Console.WriteLine("Min Elements is : "+min);
+        }
+        static void max(int[] arr)
+        {
+            int max = arr[0];
+            for(int i=1;i<arr.Length;i++)
+            {
+                if(max<arr[i])
+                {
+                    max = arr[i];
+                }
+            }
+            Console.WriteLine("Max Elements are : "+max);
+        }
+        static void Length(int[] arr)
+        {
+            int Length1 = arr.Length;
+            Console.WriteLine("The length of an array is  "+Length1);
+        }
+        static void jagged()
+        {
+            Console.WriteLine("JAGGED ARRAYS \n METHOD - 1");
+            int[][] arrays = new int[4][];
+            arrays[0] = new int[] {1,2,3,4};
+            arrays[1] = new int[] {11,24,43};
+            arrays[2] = new int[] {89,23};
+            arrays[3] = new int[] {3,5,34,221,12,45,42};
+
+            for(int i =0;i<arrays.Length;i++)
+            {
+                Console.Write("Row({0}) : \t",i);
+                for(int j =0;j<arrays[i].Length;j++)
+                {
+                    Console.Write("{0}\t",arrays[i][j]);
+                }
+                Console.WriteLine(" ");
+            }
+        }
+    }
+}
---
+++ Arrays/jagged/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace jagged
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            jagged();
+            newMethod();
+        }
+        static void jagged()
+        {
+            int[][] jagged = new int[5][];
+                new int[] {1,2,3,4};
+                new int[] {43,5,23,2};
+                new int[] {0,423,23,12,12};
+                new int[] {1,232,43,435,54};
+                new int[]{};
+            System.Console.WriteLine("The length of the jagged is : {0}",jagged.Length);
+            System.Console.WriteLine("The length off the single string : {0}",jagged[0].Length);
+            for(int i=0;i<jagged.Length;i++)
+            {
+                Console.WriteLine("Row{[0]}: ",i);
+                for(int j=0;i<jagged[i].Length;j++)
+                {
+                    Console.Write("{0}",jagged[i][j]);
+                }
+                Console.WriteLine(" ");
+            }
+        }
+        static void newMethod()
+        {
+            int a= 0;
+            for(int i =0;i<10;i++)
+            {
+                Console.Write("a : {0}\n",a);
+                a = a+5;
+            }
+            Console.Write(" ");   
+        }
+        static void Method1()
+        {
+            
+        }
+    }
+}
